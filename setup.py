e15a716dba7cc2af34f4f7d101693135daddd4c1
Restyled.io <commits@restyled.io>
2024-07-28 11:34:23 +0000

    Restyled by autopep8

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ else:
     else:
         params['scripts'] = ['bin/youtube-dl']
 
+
 class build_lazy_extractors(Command):
     description = 'Build the extractor lazy loading module'
     user_options = []
@@ -98,6 +99,7 @@ class build_lazy_extractors(Command):
             [sys.executable, 'devscripts/make_lazy_extractors.py', 'youtube_dl/extractor/lazy_extractors.py'],
             dry_run=self.dry_run,
         )
+
 
 setup(
     name='youtube_dl',
@@ -120,8 +122,8 @@ setup(
     # test_requires = ['nosetest'],
 
     # Why not try pytest? nosetest may not be compatible with older versions of python, and systems.
-     test_suite = 'pytest',
-     tests_require = ['pytest'],
+    test_suite='pytest',
+    tests_require=['pytest'],
 
 
     classifiers=[
